5a723c036c7f6c6c68c78a3019b8043d03a5af2b
bart <bart@bart.com>
2018-09-24 19:51:27 +0200

    Further nav fixes #3

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ window.onscroll = function () {
 
 var nav = document.querySelector('.js-sticky');
 var main = document.querySelector('.js-main');
+var cHead = document.querySelector('.js-head');
 var offset = nav.offsetTop;
 
 
@@ -13,9 +14,11 @@ function stickyNav() {
     if (window.pageYOffset > offset) {
         nav.classList.add('c-header--sticky');
         main.style.marginTop = navHeight + 'px';
-
+        cHead.style.padding = '0 20px';
+        
     } else {
         nav.classList.remove('c-header--sticky');
         main.style.marginTop = 0;
+        cHead.style.padding = '20px';
     }
 }
