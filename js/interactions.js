ebe7d01f90e231ba104f41b3e4bb532fb91344f5
bart <bart@bart.com>
2018-10-17 20:58:29 +0200

    Different actions for pickups, reworked played movement, added example sounds

--- js/interactions.js
+++ js/interactions.js
@@ -4,27 +4,43 @@ function findPlayer() {
     let col = parseInt(player.dataset.col);
     return [row, col]
 }
-//TODO: przepisać poruszanie za pomocą funkcji parentCol/Row i appendChild
+
 function keyPress(event) {
 
-    let playerCords = findPlayer();
-    findField(playerCords[0], playerCords[1]);
+    let player = document.querySelector('.g-bike');
+    let playerY = parseInt(parentCol(player));
+    let playerX = parseInt(parentRow(player));
+    
     if (event.keyCode == '38') {
         // up
-        if (playerCords[0] <= 0) {} else if (playerCords[0] <= game.boardHeight - 1) {
-            removeFigure('.g-bike');
-            placeFigure('g-bike', 'player', (playerCords[0] - 1), playerCords[1]);
+        if(!game.drunkState[0]){
+            playerMoveUp(playerX, playerY, player);
+        } else {
+            playerMoveDown(playerX, playerY, player);
         }
 
     } else if (event.keyCode == '40') {
         // down
-        if (playerCords[0] < game.boardHeight - 1) {
-            removeFigure('.g-bike');
-            placeFigure('g-bike', 'player', playerCords[0] + 1, playerCords[1]);
-        } else if (playerCords[0] >= game.boardHeight - 1) {
-
+        if(!game.drunkState[0]){
+            playerMoveDown(playerX, playerY, player);
+        } else {
+            playerMoveUp(playerX, playerY, player);
         }
     }
+}
+
+function playerMoveUp(x, y, player){
+    if (x <= 0) {} else if (x <= game.boardHeight - 1) {
+        let field = findField(x - 1, y);
+        field.appendChild(player);
+    }
+}
+
+function playerMoveDown(x, y, player){
+    if (x < game.boardHeight - 1) {
+        let field = findField(x + 1, y);
+        field.appendChild(player);
+    } else if (x >= game.boardHeight - 1) {}
 }
 
 function parentCol(elementClass) {
@@ -61,9 +77,50 @@ function collisionDetection() {
         let obsticleY = parentCol(x);
         if (playerX === obsticleX && playerY === obsticleY) {
             //tutaj funkcja collisionEffect()
-            game.lives--;
+            collisionEffect(x, player);
             findField(obsticleX, obsticleY).removeChild(x);
-            alert(`COLLISION! \n LIVES: ${game.lives} \n POINTS: ${game.points}`);
+            
         }
     });
+}
+
+function collisionEffect(obsticle, player) {
+    let obsticleClass = obsticle.classList[1];
+    if(obsticleClass === game.obsticles.pickupGood.obsticleClass[1]){
+        game.points += 1000;
+        playAudio('coin', 'wav');
+        console.log('moneyyyyy', game.points);
+    } else if (obsticleClass === game.obsticles.pickupGood.obsticleClass[0]){
+        if(game.lives < 3){
+            game.lives++;
+        }
+        playAudio('1-up', 'wav');
+        console.log('mushroom', game.lives);
+    } else if (obsticleClass === game.obsticles.pickupBad.obsticleClass[0]){
+        game.points - 1000;
+        playAudio('bottle', 'wav');
+        console.log('poison', game.points);
+    } else if (obsticleClass === game.obsticles.pickupBad.obsticleClass[1]){
+        game.drunkState[0] = true;
+        game.drunkState[1] = game.turn + 50;
+        player.classList.add('player-alco');
+        console.log('alco', game.drunkState, game.turn);
+        playAudio('gulp', 'mp3');
+    } else {
+        game.lives--;
+        playAudio('crash', 'mp3');
+    }
+}
+
+function alcoEffectReset(){
+    if(game.turn === game.drunkState[1]){
+        game.drunkState[0] = false;
+        let player = document.querySelector('.player');
+        player.classList.remove('player-alco');
+    }
+}
+
+function playAudio(name, format){
+    let audio = new Audio(`../sound/${name}.${format}`);
+    audio.play();
 }
